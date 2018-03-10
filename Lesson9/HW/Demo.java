3629e1b31ee0b5ee7e4212c2652ec973f39b88f5
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-10 14:08:22 +0200

    Adding HW UserClass

---
+++ Lesson9/HW/Demo.java
@@ -0,0 +1,8 @@
+package Lesson9.HW;
+
+public class Demo {
+    public static void main(String[] args) {
+        User user = new User(5, "Ihor", "55");
+        System.out.println(user.getName());
+    }
+}
