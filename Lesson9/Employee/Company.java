aa2ac42b3c955c842e68366b35011035bc7a24aa
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-10 13:53:45 +0200

    Adding HW Employee

--- Lesson9/Employee/Company.java
+++ Lesson9/Employee/Company.java
@@ -1,8 +1,8 @@
 package Lesson9.Employee;
 
 public class Company {
-    String name;
-    String countryFounded;
+    private String name;
+    private String countryFounded;
 
     protected String code;
 
