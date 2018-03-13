ee5d2e7916b02163cb4e0f8df4afbd8ccb180c4d
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-13 22:19:15 +0200

    add lesson 10. abstractbigexample

---
+++ Lesson10/abstractbigexample/BankAccount.java
@@ -0,0 +1,21 @@
+package Lesson10.abstractbigexample;
+
+import Lesson9.Employee.Employee;
+
+public class BankAccount {
+    private Employee employee;
+    private int balance;
+
+    public BankAccount(Employee employee, int balance) {
+        this.employee = employee;
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
