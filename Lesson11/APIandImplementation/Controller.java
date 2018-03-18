8a57a8208c43b70601b3273d0d92efe2d30728f0
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-18 15:22:55 +0200

    edit lesson 11. HomeWork

--- Lesson11/APIandImplementation/Controller.java
+++ Lesson11/APIandImplementation/Controller.java
@@ -12,15 +12,17 @@ public class Controller {
         int countInd = 0;
         for (API api : apis) {
             if (api != null) {
-                    countInd = api.findRooms(price,persons,city,hotel).length;
+                for (Room room : api.findRooms(price, persons, city, hotel)) {
+                    countInd++;
                 }
             }
+        }
 
         Room[] apisRes = new Room[countInd];
         int count = 0;
         for (API api : apis) {
             if (api != null) {
-                for (Room room : api.findRooms(price,persons,city,hotel)) {
+                for (Room room : api.findRooms(price, persons, city, hotel)) {
                     apisRes[count] = room;
                     count++;
                 }
@@ -30,20 +32,32 @@ public class Controller {
     }
 
 
-
     public Room[] check(API api1, API api2) {
-        Room[] resultRoom = new Room[api1.getAll().length];
-            for (int i = 0; i < api1.getAll().length; i++) {
-                for (int k = 0; k < api2.getAll().length; k++) {
-                    if (api2.getAll()[i] != null && api1.getAll()[k] != null) {
-                        if (api1.getAll()[i].getPrice() == api2.getAll()[k].getPrice() && api1.getAll()[i].getPersons() == api2.getAll()[k].getPersons()) {
-                            if (api1.getAll()[i].getHotelName().equals(api2.getAll()[k].getHotelName()) && api1.getAll()[i].getCityName().equals(api2.getAll()[k].getCityName())) {
-                                resultRoom[i] = api1.getAll()[i];
-                            }
+        int countInd = 0;
+        for (int i = 0; i < api1.getAll().length; i++) {
+            for (int k = 0; k < api2.getAll().length; k++) {
+                if (api2.getAll()[i] != null && api1.getAll()[k] != null) {
+                    if (api1.getAll()[i].getPrice() == api2.getAll()[k].getPrice() && api1.getAll()[i].getPersons() == api2.getAll()[k].getPersons()) {
+                        if (api1.getAll()[i].getHotelName().equals(api2.getAll()[k].getHotelName()) && api1.getAll()[i].getCityName().equals(api2.getAll()[k].getCityName())) {
+                       countInd++;
                         }
                     }
                 }
             }
-            return resultRoom;
+        }
+
+        Room[] resultRoom = new Room[countInd];
+        for (int i = 0; i < api1.getAll().length; i++) {
+            for (int k = 0; k < api2.getAll().length; k++) {
+                if (api2.getAll()[i] != null && api1.getAll()[k] != null) {
+                    if (api1.getAll()[i].getPrice() == api2.getAll()[k].getPrice() && api1.getAll()[i].getPersons() == api2.getAll()[k].getPersons()) {
+                        if (api1.getAll()[i].getHotelName().equals(api2.getAll()[k].getHotelName()) && api1.getAll()[i].getCityName().equals(api2.getAll()[k].getCityName())) {
+                            resultRoom[i] = api1.getAll()[i];
+                        }
+                    }
+                }
+            }
+        }
+        return resultRoom;
     }
 }
