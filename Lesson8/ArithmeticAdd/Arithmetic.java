22f0bf7021974ce4028d126c17187c8382616466
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-21 22:02:48 +0200

    Adding lessons 13.

--- Lesson8/ArithmeticAdd/Arithmetic.java
+++ Lesson8/ArithmeticAdd/Arithmetic.java
@@ -5,12 +5,15 @@ public class Arithmetic {
     public boolean check(int[] array) {
         int max = 0;
         int min = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > array[max])
-                max = i;
-            else if (array[i] < array[min])
-                min = i;
+        if (array != null) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > array[max])
+                    max = i;
+                else if (array[i] < array[min])
+                    min = i;
+            }
+            return array[max] + array[min] > 100;
         }
-        return array[max] + array[min] > 100;
+        return false;
     }
 }
