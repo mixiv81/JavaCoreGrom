d35832b899f66224cbe83c1e815bd8d38aa40568
mixiv81 <zhluktenkoiv@gmail.com>
2018-03-24 11:25:48 +0200

    adding lesson 15. Equals, hashcode

---
+++ Lesson15/HW/APIandImplementation/API.java
@@ -0,0 +1,8 @@
+package Lesson15.HW.APIandImplementation;
+
+public interface API {
+
+    Room[] findRooms(int price, int persons, String city, String hotel);
+    Room[] getAll();
+
+}
--- Lesson15/HW/APIandImplementation/Demo.java
+++ Lesson15/HW/APIandImplementation/Demo.java
@@ -1,28 +1,27 @@
-package Lesson13;
-
-import Lesson11.APIandImplementation.*;
+package Lesson15.HW.APIandImplementation;
 
 import java.util.Arrays;
 import java.util.Date;
 
-public class L11 {
+public class Demo {
     public static void main(String[] args) {
         Room room1 = new Room(1581, 2000, 4, new Date(), "Dnepr", "Kiev");
         Room room2 = new Room(1582, 2800, 1, new Date(), "Hilton", "Kiev");
         Room room3 = new Room(1583, 2000, 4, new Date(), "Dnepr", "Kiev");
         Room room4 = new Room(1584, 2500, 3, new Date(), "Kiev", "Kiev");
         Room room5 = new Room(1585, 2800, 1, new Date(), "Hilton", "Kiev");
-        Room room6 = new Room(1585, 2800, 1, new Date(), null, "Kiev");
+        Room room6 = new Room(1585, 2800, 1, new Date(), "Hilton", "Kiev");
+
         Room[] rooms = new Room[]{room1, room2};
         Room[] rooms1 = new Room[]{room3, room4};
         Room[] rooms2 = new Room[]{room5, room6};
+
+
         BookingComAPI bookingComAPI = new BookingComAPI(rooms);
         TripAdvisorAPI tripAdvisorAPI = new TripAdvisorAPI(rooms1);
         GoogleAPI googleAPI = new GoogleAPI(rooms2);
         API[] apis = new API[]{bookingComAPI, tripAdvisorAPI, googleAPI};
         Controller controller = new Controller(apis);
-
-
 
         System.out.println("Check");
         System.out.println(Arrays.toString(controller.check(bookingComAPI, googleAPI)));
